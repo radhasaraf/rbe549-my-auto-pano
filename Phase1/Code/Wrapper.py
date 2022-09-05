f84cce6fc37c8e84baa1fb95d09ef44d47f262b9
Ramana <saikrn112@gmail.com>
2022-09-04 21:57:37 -0400

    vectorize for image sequences

--- Phase1/Code/Wrapper.py
+++ Phase1/Code/Wrapper.py
@@ -24,10 +24,16 @@ import cv2
 def main():
     # Add any Command Line arguments here
     Parser = argparse.ArgumentParser()
-    Parser.add_argument('--NumFeatures', default=100, help='Number of best features to extract from each image, Default:100')
+    Parser.add_argument('--CorerHarrisBlockSize', default=4, help='Block Size for Harris Corner Detection, Default:4')
+    Parser.add_argument('--CornerHarrisSobelOpSize', default=5, help='Block Size for Harris Corner Detection, Default:5')
+    Parser.add_argument('--CornerHarrisKParameter', default=0.04, help='Block Size for Harris Corner Detection, Default:0.04')
+    Parser.add_argument('--ANMSLocalMaximaThreshold', default=0.01, help='ANMS Local maxima threshold, Default:0.01')
 
     Args = Parser.parse_args()
-    NumFeatures = Args.NumFeatures
+    ch_block_size = Args.CornerHarrisBlockSize
+    ch_ksize = Args.CornerHarrisSobelOpSize
+    ch_k = Args.CornerHarrisKParameter
+    ansm_local_maxima_threshold = Args.ANMSLocalMaximaThreshold
 
     """
     Read a set of images for Panorama stitching
