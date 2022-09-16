cea38650fde1e222b62c7440c89ddefba2df1f56
Ramana <pinnama.ramana@gmail.com>
2022-09-16 14:22:52 -0400

    phase I working till individual image correspondences

--- Phase1/Code/Wrapper.py
+++ Phase1/Code/Wrapper.py
@@ -27,16 +27,12 @@ from matplotlib import pyplot as plt
 from skimage.feature import corner_peaks,peak_local_max
 from typing import List
 import math
-# D:/conda/envs/CV\lib\site-packages\matplotlib-3.5.3-py3.8-nspkg.pth
 import random
 import warnings
 from shapely.geometry import Point,Polygon
 
 
 # Helper funcs
-def cvt_for_plt(img):
-    return cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-
 def normalize(image_mat):
     return cv2.normalize(image_mat,dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
 
@@ -54,24 +50,27 @@ def write_image_output(suffix,file_name,extension,image,path=None,display=False)
     else:
         cv2.imwrite(output_file_path,image)
 
-def calc_and_print_stats(suffix, image):
+def calc_and_print_stats(image):
     arr = image.flatten()
     maxi = max(arr)
     mini = min(arr)
-    avg = np.average(arr)
-    stddev = np.std(arr)
-    # print(f"{suffix} mean:{avg}")
-    # print(f"{suffix} max:{maxi}")
-    # print(f"{suffix} min:{mini}")
-    # print(f"{suffix} dev:{stddev}")
+    try:
+        avg = np.average(arr)
+        stddev = np.std(arr,dtype=np.float32)
+    except TypeError:
+        print(f"error arr:{image.dtype}") 
+        return [1,0,0,0]
     return [max,min,avg,stddev]
 
 def standardize_image(image,debug=False):
     if debug:
         print(type(image))
-    mean = np.mean(image,keepdims=True)
-    std = np.sqrt((image - mean)**2).mean(keepdims=True)
-    return (image - mean)/std
+    try:
+        mean = np.mean(image,dtype=np.float32)
+    except TypeError:
+        return False,image
+    std = np.sqrt((image - mean)**2).mean()
+    return True,(image - mean)/std
 
 def draw_markers(suffix,image,coords,color,display=False,file_name=None,output_file_extension=".png",path=None):
     image_dup = np.copy(image)
@@ -85,53 +84,15 @@ def draw_markers(suffix,image,coords,color,display=False,file_name=None,output_f
             raise ValueError("file_name or path is None when saving the image")
         write_image_output(suffix,file_name,output_file_extension,image_dup,path)
 
-def make_subplots(row_length,images):
-    print(len(images))
-    if len(images)%5 == len(images):
-        fig,axs = plt.subplots(len(images))
-        fig.set_size_inches(16,16)
-        axs = axs.ravel()    
-    else:
-        fig,axs = plt.subplots(5,int(len(images)/5))
-        fig.set_size_inches(16,16)
-        axs = axs.ravel()    
-
-    for i,image in enumerate(images):
-            axs[i].imshow(cvt_for_plt(image))
-
-def embed_corners(img,corner_score_img=None,points=None):
-    if corner_score_img is None and points is None:
-        return
-    img_corners = np.copy(img)
-    if corner_score_img is not None:
-        corner_score_img1 = cv2.dilate(corner_score_img,None)
-        img_corners[corner_score_img1>0.01*corner_score_img1.max()] = [0,0,255]
-        print(np.sum(corner_score_img1>0.01*corner_score_img1.max()))
-    elif points is not None:
-        print(np.shape(points))
-        img_corners[tuple(points.T)] = [0,0,255]
-    return img_corners
-
-def embed_and_plot_corners(color_images,images_corner_score=None,images_corner_points=None):
-    if images_corner_score is None and images_corner_points is None:
-        return
-    corner_embed_images = []
-    if images_corner_score is not None:
-        for i in range(len(color_images)):
-            corner_embed_images.append(embed_corners(color_images[i],corner_score_img=images_corner_score[i]))
-    elif images_corner_points is not None:
-        for i in range(len(color_images)):
-            corner_embed_images.append(embed_corners(color_images[i],points=images_corner_points[i]))
-    make_subplots(5,corner_embed_images)
 
 ## ANMS
 from functools import total_ordering
 
 def get_corners(img,bsize,ksize,k,file_name,output_extn,output_path):
     corners_of_img = cv2.cornerHarris(img,bsize,ksize,k) ### TODO need to try out Shi-Tomasi Corner Detection instead
-    corners_of_img = standardize_image(corners_of_img)
+    _,corners_of_img = standardize_image(corners_of_img)
     n_harris_corners = np.sum(corners_of_img>0.01*corners_of_img.max())
-    [_,_,_,stddev] = calc_and_print_stats(file_name,corners_of_img)
+    [_,_,_,stddev] = calc_and_print_stats(corners_of_img)
     
     write_image_output("pure_corners",
             file_name,
@@ -176,11 +137,21 @@ class CoOrds:
 def apply_anms_to_img(local_maxima_coords,corner_score_img,n_best):
     "input: "
     "output: numpy array of coordinates"
+    return local_maxima_coords
+    if len(local_maxima_coords) <=0:
+        return local_maxima_coords
+
+    assert(len(local_maxima_coords[0]) == 2)
 
     # initializing distances array
     distances = np.full(local_maxima_coords.shape[0],fill_value=CoOrds(0,0))
     for i,coords in enumerate(local_maxima_coords):
-        distances[i] = CoOrds(coords[0],coords[1])
+        if len(coords) < 2:
+            print(coords)
+            continue
+        x,y = coords
+        ret = CoOrds(x,y)
+        distances[i] = ret
 
     ED = None
     for i,coord_i in enumerate(local_maxima_coords):
@@ -222,12 +193,18 @@ def get_corner_descriptors(img_gray: List[List], corner_locs: np.array(List[List
         corner_patch = corner_patch.flatten()[::25]
         corner_patch = np.reshape(corner_patch,newshape=(8,8))
         # print(corner_patch.shape)
-        # print(corner_patch)
-        corner_patch = standardize_image(corner_patch,False)
+        is_good, corner_patch = standardize_image(corner_patch,False)
+        if not is_good:
+            continue
         # corner_patch = cv2.GaussianBlur(corner_patch, (3, 3), 1)
         feature_descriptor = FeatureDescriptor(x,y,corner_patch.flatten())
         corner_descriptors.append(feature_descriptor)
-    return corner_descriptors
+#    try:
+#    except:
+#        print("something went wrong")
+#        return False, []    
+    print(len(corner_descriptors))
+    return True, corner_descriptors
 
 def draw_random_corner_patch(img_fds,file_names,output_extension,output_path,display=False):
     rand_img_i = random.randint(0,len(file_names)-1)
@@ -263,7 +240,7 @@ def get_feature_matches(img1_descriptor: List[FeatureDescriptor], img2_descripto
     keypoints1 = np.array(keypoints1)
     keypoints2 = np.array(keypoints2)
     keypoint_distances = np.array(keypoint_distances) 
-
+    print(len(keypoints1))
     return [keypoints1,keypoints2,keypoint_distances]
 
 def draw_feature_matches(keypoints1,keypoints2,keypoints_distances,first_image,second_image,ref_file_id,to_file_id,output_file_extension,output_path,suffix=""):
@@ -405,8 +382,9 @@ def affine_and_resize_image(image,translation_vec,new_shape):
     tf_ref_img = cv2.warpAffine(image,affine_mat,[final_canvas_shape[1],final_canvas_shape[0]])
     return tf_ref_img,new_bb_coords,final_canvas_shape
 
-def stitch_images(ref_image,to_image,Args,ref_file_id,to_file_id):
-    print(f"\n\n Running stitch_images for {ref_file_id}<-{to_file_id}")
+def get_image_info(ref_image_orig,Args,ref_file_id):
+    print(f"\n\n Running get_image_info for {ref_file_id}")
+    ref_image = ref_image_orig.copy()
     ch_block_size = Args.CornerHarrisBlockSize
     ch_ksize = Args.CornerHarrisSobelOpSize
     ch_k = Args.CornerHarrisKParameter
@@ -420,8 +398,6 @@ def stitch_images(ref_image,to_image,Args,ref_file_id,to_file_id):
     out_path = base_path + img_set
 
     ref_img_gray = cv2.cvtColor(ref_image,cv2.COLOR_BGR2GRAY)
-    to_img_gray = cv2.cvtColor(to_image,cv2.COLOR_BGR2GRAY)
-    
 
     """
     Corner Detection
@@ -434,24 +410,13 @@ def stitch_images(ref_image,to_image,Args,ref_file_id,to_file_id):
                                 ref_file_id,
                                 output_file_extension,
                                 out_path)
+    ref_img_corners_coords = np.array(np.nonzero(ref_img_corners)).T
 
-    (to_img_corners,to_corner_count,to_stddev) = get_corners(to_img_gray,
-                                ch_block_size,
-                                ch_ksize,
-                                ch_k,
-                                to_file_id,
-                                output_file_extension,
-                                out_path)
-   
 
     """
     local maxima
     """
-    min_stddev = None
-    if ref_stddev < to_stddev:
-        min_stddev = ref_stddev
-    else:
-        min_stddev = to_stddev
+    min_stddev = ref_stddev
 
     ref_local_max_corners,ref_local_max_corners_coords = apply_corners_local_maxima(
                                 ref_img_corners,
@@ -460,28 +425,12 @@ def stitch_images(ref_image,to_image,Args,ref_file_id,to_file_id):
                                 ref_file_id,
                                 output_file_extension,
                                 out_path)
+
     draw_markers("embed_max_corners",
                 ref_image,
                 ref_local_max_corners_coords,
                 color=[0,255,0],
                 file_name=ref_file_id,
-                output_file_extension=output_file_extension,
-                path=out_path,
-                display=False)
-
-    
-    to_local_max_corners,to_local_max_corners_coords = apply_corners_local_maxima(
-                                to_img_corners,
-                                min_stddev,
-                                local_maxima_stddev_threshold_factor,
-                                to_file_id,
-                                output_file_extension,
-                                out_path)
-    draw_markers("embed_max_corners",
-                to_image,
-                to_local_max_corners_coords,
-                color=[0,255,0],
-                file_name=to_file_id,
                 output_file_extension=output_file_extension,
                 path=out_path,
                 display=False)
@@ -493,6 +442,7 @@ def stitch_images(ref_image,to_image,Args,ref_file_id,to_file_id):
     ref_anms_coords = apply_anms_to_img(ref_local_max_corners_coords,
                                 ref_local_max_corners,
                                 max_features)
+    print(f"ref:{len(ref_anms_coords)}")
 
     draw_markers("anms_corners",
             ref_image,
@@ -504,25 +454,35 @@ def stitch_images(ref_image,to_image,Args,ref_file_id,to_file_id):
             display=False)
 
 
-    to_anms_coords = apply_anms_to_img(to_local_max_corners_coords,
-                                to_local_max_corners,
-                                max_features)
-    
-    draw_markers("anms_corners",
-            to_image,
-            to_anms_coords,
-            color=[0,255,0],
-            file_name=to_file_id,
-            output_file_extension=output_file_extension,
-            path=out_path,
-            display=False)
-   
     """
     Feature Descriptors
     Save Feature Descriptor output as FD.png
     """
-    ref_fds = get_corner_descriptors(ref_img_gray, ref_anms_coords)
-    to_fds = get_corner_descriptors(to_img_gray, to_anms_coords)
+    
+    _, ref_fds = get_corner_descriptors(ref_img_gray, ref_local_max_corners_coords)
+
+    if len(ref_fds) <= 0:
+        print(ref_img_corners_coords)
+        print("no feture_descriptors")
+        return False,None
+
+    return True,ref_fds
+
+def get_img_correspondences(ref_image_orig, to_image_orig, ref_fds,to_fds,Args,ref_file_id,to_file_id):
+    print(f"\n\n Running get_img_correspondences for {ref_file_id}<-{to_file_id}")
+    ref_image = ref_image_orig.copy()
+    to_image = to_image_orig.copy()
+    ch_block_size = Args.CornerHarrisBlockSize
+    ch_ksize = Args.CornerHarrisSobelOpSize
+    ch_k = Args.CornerHarrisKParameter
+    n_max_ransac_iterations = Args.RansacMaxIterations
+    local_maxima_stddev_threshold_factor = Args.StdDevThresholdFactorForLocalMaxima
+    max_features =  Args.ANMSMaxFeatures
+    base_path = Args.BasePath
+    img_set = Args.TestSet
+    input_file_extension = '.jpg'
+    output_file_extension = '.png'
+    out_path = base_path + img_set
 
     """
     Feature Matching
@@ -564,7 +524,9 @@ def stitch_images(ref_image,to_image,Args,ref_file_id,to_file_id):
     
     H = find_homography(ref_kps,to_kps,max_inliers)
 
-    return H,np.sum(max_inliers)
+    return True, H,np.sum(max_inliers)
+
+def stitch_images(ref_image,to_image,H):
     """
     Image Warping + Blending
     Save Panorama output as mypano.png
@@ -651,24 +613,53 @@ def main():
 
     images_color = np.array(images_color)
 
-    # ref_image = images_color[1]
-    # ref_file_id = file_names[1]
+    """
+    Get all the needed information like feature descriptors
+    """
+    fds = []
+    for i in range(len(files)):
+        isGood, img_fds = get_image_info(images_color[i],Args,file_names[i])
+        fds.append([isGood, img_fds])
 
+    """
+    get image correspondences of all possible pairs
+    store them in adjacency matrix to construct a best possible graph
+    """
     imgs_graph = np.zeros(shape=(len(files),len(files)))
     homography_mats_list = []
     homography_inds_mat = np.zeros(shape=(len(files),len(files)))
     for i in range(len(files)):
+        if not fds[i][0]:
+            continue
         for j in range(len(files)):
+            if not fds[j][0]:
+                continue
             if i == j:
                 continue
             print(f"\n\nRunning Iteration for {i},{j}")
-            H, inliers = stitch_images(images_color[i],images_color[j],Args,file_names[i],file_names[j])
+            isGood, H, inliers = get_img_correspondences(images_color[i],images_color[j],fds[i][1],fds[j][1],Args,file_names[i],file_names[j])
+            if not isGood:
+                continue
+            print(f"inliers:{inliers}")
             imgs_graph[i,j] = inliers
             homography_mats_list.append(H)
             homography_inds_mat[i,j] = len(homography_mats_list)
 
     print(imgs_graph)
     print(homography_inds_mat)
+
+
+    """
+    Recognizing panorama images
+    """
+    """
+    Step: 1
+        Identify directions of the panorama graph
+        from homography matrix
+    """
+#    for i in range(len(files)):
+#        for j in range(k
+
 
     # stitch_images(ref_image,images_color[2],Args,ref_file_id,file_names[2])
     # return
